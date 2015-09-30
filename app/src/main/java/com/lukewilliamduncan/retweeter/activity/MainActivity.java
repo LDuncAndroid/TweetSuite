2d1877abea85c37ece7e6f1ce555df1334dec8e7
Luke Duncan <l.duncan@duncanfishertech.com>
2015-09-30 11:29:49 +0100

    Added Butterknife for View Binding. Added method in MainActivity to set the title of the Toolbar before setting the Toolbar as the SupportActionBar.

--- app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
@@ -2,14 +2,30 @@ package com.lukewilliamduncan.retweeter.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.lukewilliamduncan.retweeter.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolBar)
+    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        mToolbar.setTitle(getString(R.string.app_name));
+        setSupportActionBar(mToolbar);
     }
 }
