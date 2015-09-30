7090ceb86e88c0d4009bfaa59e2d4f0e0d5e2b64
Luke Duncan <l.duncan@duncanfishertech.com>
2015-09-30 13:05:07 +0100

    Moving MainActivity to new package

--- app/src/main/java/com/lukewilliamduncan/retweeter/activity/activity/MainActivity.java
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.lukewilliamduncan.retweeter.activity;
+package com.lukewilliamduncan.retweeter.activity.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
