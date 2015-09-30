59f47845dd204f3affb1917c23e717804b553c9b
Luke Duncan <l.duncan@duncanfishertech.com>
2015-09-30 13:02:20 +0100

    Added TabLayout to hold tabs for search terms. Added ViewPager with FragmentStatePagerAdapter and setup the TabLayout with the ViewPager.

--- app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
@@ -1,6 +1,11 @@
 package com.lukewilliamduncan.retweeter.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,13 +13,28 @@ import android.view.MenuItem;
 
 import com.lukewilliamduncan.retweeter.R;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Views
+     */
     @Bind(R.id.toolBar)
     Toolbar mToolbar;
+
+    @Bind(R.id.tabLayout)
+    TabLayout mTabLayout;
+
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+
+    private final ArrayList<String> mSearchTerms = new ArrayList<>();
+
+    private TweetSearchTabPagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupToolbar();
+        setupViewPager();
+        setupTabLayout();
     }
 
     @Override
@@ -45,5 +67,36 @@ public class MainActivity extends AppCompatActivity {
     private void setupToolbar() {
         mToolbar.setTitle(getString(R.string.app_name));
         setSupportActionBar(mToolbar);
+    }
+
+    private void setupViewPager() {
+        mPagerAdapter = new TweetSearchTabPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
+    }
+
+    private void setupTabLayout() {
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    private class TweetSearchTabPagerAdapter extends FragmentStatePagerAdapter {
+
+        public TweetSearchTabPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mSearchTerms.size();
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            //TODO - Return fragment containing list of tweets pertaining to search term
+            return new Fragment();
+        }
+
+        public int getItemPosition(Object object) {
+            return POSITION_NONE;
+        }
     }
 }
