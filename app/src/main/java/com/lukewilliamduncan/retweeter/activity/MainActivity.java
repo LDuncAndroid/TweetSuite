b3150b34b6a46505e421249e29efc9b4835fbd66
Luke Duncan <l.duncan@duncanfishertech.com>
2015-09-30 10:40:36 +0100

    Added blank MainActivity

---
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lukewilliamduncan.retweeter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.lukewilliamduncan.retweeter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
