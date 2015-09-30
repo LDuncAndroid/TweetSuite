1fdef4d425d57c678a14a16991419b2357c1112e
Luke Duncan <l.duncan@duncanfishertech.com>
2015-09-30 14:46:21 +0100

    Created View for user to enter search terms. View controls keyboard's visibility and requests focus when visible and clears focus when not.
    Created ViewUtil class, currently contains methods to show and hide keyboard.

---
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/SearchEditText.java
@@ -0,0 +1,62 @@
+package com.lukewilliamduncan.retweeter.activity;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.lukewilliamduncan.retweeter.activity.util.ViewUtils;
+
+/**
+ * Created by luke on 30/09/15.
+ */
+public class SearchEditText extends EditText implements TextView.OnEditorActionListener {
+
+    private OnSearchPressedListener mOnSearchPressedListener;
+
+    public SearchEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        setSingleLine();
+        setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        setOnEditorActionListener(this);
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+            if (mOnSearchPressedListener != null) {
+                mOnSearchPressedListener.searchPressed(String.valueOf(getText()));
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onVisibilityChanged(View changedView, int visibility) {
+        if (!isInEditMode()) {
+            if (visibility == View.VISIBLE) {
+                requestFocus();
+                ViewUtils.showKeyboard(getContext(), this);
+            } else {
+                clearFocus();
+                ViewUtils.hideKeyboard(getContext(), this);
+            }
+        }
+    }
+
+    public void setOnSearchPressedListener(OnSearchPressedListener listener) {
+        mOnSearchPressedListener = listener;
+    }
+
+    public interface OnSearchPressedListener {
+        void searchPressed(String searchTerm);
+    }
+
+}
---
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/util/ViewUtils.java
@@ -0,0 +1,26 @@
+package com.lukewilliamduncan.retweeter.activity.util;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * Created by luke on 30/09/15.
+ */
+public class ViewUtils {
+
+    public static void showKeyboard(Context context, View view) {
+        if (context != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+            // only will trigger it if no physical keyboard is open
+            inputMethodManager.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    public static void hideKeyboard(Context context, View view) {
+        InputMethodManager imm = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+}
