2bdbeae8a88c134515616c497c7bce2b13855ec2
Luke Duncan <l.duncan@duncanfishertech.com>
2015-09-30 12:45:36 +0100

    Added Menu Item to add new tabs when pressed.

--- app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
+++ app/src/main/java/com/lukewilliamduncan/retweeter/activity/MainActivity.java
@@ -3,6 +3,8 @@ package com.lukewilliamduncan.retweeter.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.lukewilliamduncan.retweeter.R;
 
@@ -22,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new_tab:
+                //TODO - Handle add new tab press
+                return true;
+        }
+        return false;
     }
 
     private void setupToolbar() {
