8721a76a3adb3880d78fc00b9a87b848d7041f1b
Luke Duncan <l.duncan@duncanfishertech.com>
2015-10-02 12:06:22 +0100

    Created view layout for Tweet. Using Picasso to load User profile images.

--- app/src/main/java/com/lukewilliamduncan/retweeter/adapter/TweetListAdapter.java
+++ app/src/main/java/com/lukewilliamduncan/retweeter/adapter/TweetListAdapter.java
@@ -1,15 +1,23 @@
 package com.lukewilliamduncan.retweeter.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.lukewilliamduncan.retweeter.R;
 import com.lukewilliamduncan.retweeter.model.Tweet;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by luke on 02/10/15.
@@ -18,10 +26,12 @@ public class TweetListAdapter extends BaseAdapter {
 
     private final Context mContext;
     private List<Tweet> mTweets;
+    private final ArrayList<Long> mTweetsIds;
 
     public TweetListAdapter(Context context) {
         mContext = context;
         mTweets = new ArrayList<>();
+        mTweetsIds = new ArrayList<>();
     }
 
     @Override
@@ -46,16 +56,48 @@ public class TweetListAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh;
+
+        if (convertView == null) {
+            LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+            convertView = inflater.inflate(R.layout.tweet_view, parent, false);
+            vh = new ViewHolder(convertView);
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
         Tweet tweet = mTweets.get(position);
-        TextView tv = new TextView(mContext);
+        vh.userName.setText(tweet.getUser().getName());
+        vh.message.setText(tweet.getText());
 
-        tv.setText(tweet.getText());
+        Picasso.with(mContext)
+                .load(tweet.getUser().getProfileImageUrl())
+                .noPlaceholder()
+                .resize(500, 500)
+                .onlyScaleDown()
+                .into(vh.userImage);
 
-        return tv;
+        return convertView;
     }
 
     public void addItem(Tweet tweet) {
-        mTweets.add(tweet);
-        notifyDataSetChanged();
+        if (!mTweetsIds.contains(tweet.getId())) {
+            mTweets.add(tweet);
+            notifyDataSetChanged();
+        }
+    }
+
+    static class ViewHolder {
+        @Bind(R.id.tweetUser)
+        TextView userName;
+        @Bind(R.id.tweetMessage)
+        TextView message;
+        @Bind(R.id.tweetUserImage)
+        ImageView userImage;
+
+        public ViewHolder(View convertView) {
+            ButterKnife.bind(this, convertView);
+        }
     }
 }
